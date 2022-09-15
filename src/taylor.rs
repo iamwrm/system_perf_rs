167d47283f35da8dfab75ea0b387bbb3b8887c4f
iamwrm <iamwrm@gmail.com>
2022-09-15 12:59:58 +0000

    test use macro

--- src/taylor.rs
+++ src/taylor.rs
@@ -1,23 +1,32 @@
 /// <https://people.math.sc.edu/girardi/m142/handouts/10sTaylorPolySeries.pdf>
 ///
 ///
-
+use is_odd::IsOdd;
 
 #[cfg(test)]
 use more_asserts as ma;
 
+macro_rules! series_test {
+    ($series_func:expr,$original:expr,$test_func_name:ident) => {
+        #[test]
+        fn $test_func_name() {
+            let x = 0.1f64;
+            let sum: f64 = (0..100i32).map(|n| $series_func(x, n)).sum();
+            ma::assert_le!((sum - $original(x)).abs(), 1e-5f64);
+        }
+    };
+}
 
 /// 1/(1-x) = 1 + x + x^2 + ... + x^n
 /// x ∈ (-1, 1)
 pub fn series_1_over_1mx(x: f64, n: i32) -> f64 {
     x.powi(n)
 }
-#[test]
-fn test_series_1over_1mx() {
-    let x = 0.1f64;
-    let sum: f64 = (0..100i32).map(|n| series_1_over_1mx(x, n)).sum();
-    ma::assert_le!((sum - 1f64 / (1f64 - x)).abs(), 1e-5f64);
-}
+series_test!(
+    series_1_over_1mx,
+    |x| { 1f64 / (1f64 - x) },
+    test_series_1_over_1mx
+);
 
 /// e^x = 1 + x + x^2/2! + ... + x^n/n!
 /// x ∈ R
@@ -32,14 +41,7 @@ pub fn series_e(x: f64, n: i32) -> f64 {
     };
     up / down
 }
-#[test]
-fn test_series_e() {
-    let x = 0.1f64;
-    let sum: f64 = (0..100i32).map(|n| series_e(x, n)).sum();
-    ma::assert_le!((sum - f64::exp(x)).abs(), 1e-5f64);
-}
-
-use is_odd::IsOdd;
+series_test!(series_e, |x| { f64::exp(x) }, test_series_e);
 
 /// cos(x) = 1 - x^2/2! + x^4/4! - ... + (-1)^n * x^(2n)/(2n)!
 /// x ∈ R
@@ -58,9 +60,4 @@ pub fn series_cos(x: f64, n: i32) -> f64 {
     }
     ans
 }
-#[test]
-fn test_series_cos() {
-    let x = 0.5f64;
-    let sum: f64 = (0..10i32).map(|n| series_cos(x, n)).sum();
-    ma::assert_le!((sum - f64::cos(x)).abs(), 1e-5f64);
-}
+series_test!(series_cos, |x| { f64::cos(x) }, test_series_cos);
