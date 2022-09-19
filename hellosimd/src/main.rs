6866eed1d8fda4e87cb022cb84db23cd674f3bc6
Ren Wang <iamwrm@gmail.com>
2022-09-19 03:15:20 +0000

    still doesn't use avx512

--- hellosimd/src/main.rs
+++ hellosimd/src/main.rs
@@ -1,7 +1,17 @@
 #![feature(portable_simd)]
 use core_simd::*;
 fn main() {
-    let a = f32x4::splat(10.0);
-    let b = f32x4::from_array([1.0, 2.0, 3.0, 4.0]);
+    let a = f32x16::splat(10.0);
+    let arr = {
+        let mut arr = [0.0; 16];
+        for i in 0..16 {
+            arr[i] = i as f32;
+        }
+        arr
+    };
+    let mut b = f32x16::from_array(arr);
+    for _ in 0..100 {
+        b = a + b;
+    }
     println!("{:?}", a + b);
 }
