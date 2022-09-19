c5c179b18192dfa948da8a3a260e9808c5df9567
Ren Wang <iamwrm@gmail.com>
2022-09-19 04:46:10 +0000

    scaler finish

--- hellosimd/src/main.rs
+++ hellosimd/src/main.rs
@@ -1,6 +1,48 @@
 #![feature(portable_simd)]
 use core_simd::*;
+
+use std::time;
+
 fn main() {
+    // run_simd1();
+    let t1 = time::Instant::now();
+    run_simd2();
+    println!("simd2: {:?}", t1.elapsed());
+}
+
+fn black_box<T>(dummy: T) -> T {
+    unsafe { std::ptr::read_volatile(&dummy) }
+}
+
+fn scaler(a: &mut Vec<f32>) {
+    // add x for 100 times
+    for i in 0..10_000_000 {
+        a.iter_mut().for_each(|x| {
+            let i = i as f32;
+            *x += i;
+        });
+    }
+
+    black_box(a[1023]);
+}
+
+fn get_vec() -> Vec<f32> {
+    let mut a = Vec::with_capacity(1024);
+    for i in 0..1024 {
+        a.push(i as f32);
+    }
+    a
+}
+
+fn run_simd2() {
+    let mut a = get_vec();
+
+    scaler(&mut a);
+    println!("a[{}] = {}", 1023, a[1023]);
+}
+
+#[allow(dead_code)]
+fn run_simd1() {
     let a = f32x16::splat(10.0);
     let arr = {
         let mut arr = [0.0; 16];
