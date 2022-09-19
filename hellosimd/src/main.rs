adc2696ac85cf56baaa508d233630bf699f7cf2a
Ren Wang <iamwrm@gmail.com>
2022-09-19 06:18:53 +0000

    add more simd

--- hellosimd/src/main.rs
+++ hellosimd/src/main.rs
@@ -46,17 +46,74 @@ where
 
 fn run_simd2(arr_length: u64, iter_time: u64) {
     bench(arr_length, scalar, "scalar", iter_time);
-    bench(arr_length, simd_vector, "simd", iter_time);
+    bench(arr_length, simd_vector_4, "simd_4", iter_time);
+    bench(arr_length, simd_vector_8, "simd_8", iter_time);
+    bench(arr_length, simd_vector_16, "simd_16", iter_time);
 }
 
-fn simd_vector(a: &mut Vec<f32>, n_max: u64) {
-    let n = a.len() / 16;
+fn simd_vector_4(a: &mut Vec<f32>, n_max: u64) {
+    const SIMD_WIDTH: usize = 4;
+    let n = a.len() / SIMD_WIDTH;
 
     for simd_slice in 0..n {
-        let a_slice = &mut a[simd_slice * 16..(simd_slice + 1) * 16];
+        let a_slice = &mut a[simd_slice * SIMD_WIDTH..(simd_slice + 1) * SIMD_WIDTH];
+        let mut a16 = f32x4::from_array(a_slice.try_into().unwrap());
+
+        let k = (simd_slice * SIMD_WIDTH..(simd_slice + 1) * SIMD_WIDTH)
+            .into_iter()
+            .map(|i| i as f32)
+            .collect::<Vec<f32>>();
+        let k16 = f32x4::from_array(k.try_into().unwrap());
+
+        for _ in 0..n_max {
+            a16 = a16 + k16;
+        }
+        a_slice.copy_from_slice(&a16.to_array());
+    }
+
+    for j in n * SIMD_WIDTH..a.len() {
+        for _ in 0..n_max {
+            a[j] += j as f32;
+        }
+    }
+}
+
+fn simd_vector_8(a: &mut Vec<f32>, n_max: u64) {
+    const SIMD_WIDTH: usize = 8;
+    let n = a.len() / SIMD_WIDTH;
+
+    for simd_slice in 0..n {
+        let a_slice = &mut a[simd_slice * SIMD_WIDTH..(simd_slice + 1) * SIMD_WIDTH];
+        let mut a16 = f32x8::from_array(a_slice.try_into().unwrap());
+
+        let k = (simd_slice * SIMD_WIDTH..(simd_slice + 1) * SIMD_WIDTH)
+            .into_iter()
+            .map(|i| i as f32)
+            .collect::<Vec<f32>>();
+        let k16 = f32x8::from_array(k.try_into().unwrap());
+
+        for _ in 0..n_max {
+            a16 = a16 + k16;
+        }
+        a_slice.copy_from_slice(&a16.to_array());
+    }
+
+    for j in n * SIMD_WIDTH..a.len() {
+        for _ in 0..n_max {
+            a[j] += j as f32;
+        }
+    }
+}
+
+fn simd_vector_16(a: &mut Vec<f32>, n_max: u64) {
+    const SIMD_WIDTH: usize = 16;
+    let n = a.len() / SIMD_WIDTH;
+
+    for simd_slice in 0..n {
+        let a_slice = &mut a[simd_slice * SIMD_WIDTH..(simd_slice + 1) * SIMD_WIDTH];
         let mut a16 = f32x16::from_array(a_slice.try_into().unwrap());
 
-        let k = (simd_slice * 16..(simd_slice + 1) * 16)
+        let k = (simd_slice * SIMD_WIDTH..(simd_slice + 1) * SIMD_WIDTH)
             .into_iter()
             .map(|i| i as f32)
             .collect::<Vec<f32>>();
@@ -68,7 +125,7 @@ fn simd_vector(a: &mut Vec<f32>, n_max: u64) {
         a_slice.copy_from_slice(&a16.to_array());
     }
 
-    for j in n * 16..a.len() {
+    for j in n * SIMD_WIDTH..a.len() {
         for _ in 0..n_max {
             a[j] += j as f32;
         }
