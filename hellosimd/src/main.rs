368f046835a4591da307d397204f917285a51ce4
Ren Wang <iamwrm@gmail.com>
2022-09-19 05:15:56 +0000

    simd is slower for now

--- hellosimd/src/main.rs
+++ hellosimd/src/main.rs
@@ -12,24 +12,10 @@ fn black_box<T>(dummy: T) -> T {
     unsafe { std::ptr::read_volatile(&dummy) }
 }
 
-fn scaler(a: &mut Vec<f32>, i: i32) {
-    a.iter_mut().for_each(|x| {
-        let i = i as f32;
-        *x += i;
-    });
-}
-
-fn simd_vector(a: &mut Vec<f32>, i: i32) {
-    // add x for 100 times
-    a.iter_mut().for_each(|x| {
-        let i = i as f32;
-        *x += i;
-    });
-}
-
 fn get_vec() -> Vec<f32> {
-    let mut a = Vec::with_capacity(1024);
-    for i in 0..1024 {
+    let cap = 1000;
+    let mut a = Vec::with_capacity(cap);
+    for i in 0..cap {
         a.push(i as f32);
     }
     a
@@ -44,15 +30,15 @@ where
     for i in 0..10_000_000 {
         f_to_bench(&mut a, i);
     }
-    println!("a[{}] = {}", 1023, a[1023]);
-    black_box(a[1023]);
+    let idx = 999;
+    println!("a[{}] = {}", idx, a[idx]);
+    black_box(a[idx]);
     println!("simd2: {:?}", t1.elapsed());
 }
 
 fn run_simd2() {
     bench(scaler);
-    bench(scaler);
-    bench(scaler);
+    bench(simd_vector);
 }
 
 #[allow(dead_code)]
@@ -70,4 +56,26 @@ fn run_simd1() {
         b = a + b;
     }
     println!("{:?}", a + b);
+}
+
+fn simd_vector(a: &mut Vec<f32>, i: i32) {
+    let n = a.len() / 16;
+
+    for j in 0..n {
+        let a_slice = &mut a[j * 16..(j + 1) * 16];
+        let mut a16 = f32x16::from_array(a_slice.try_into().unwrap());
+        let i = f32x16::splat(i as f32);
+        a16 = a16 + i;
+        a_slice.copy_from_slice(&a16.to_array());
+    }
+
+    for j in n * 16..a.len() {
+        a[j] += i as f32;
+    }
+}
+fn scaler(a: &mut Vec<f32>, i: i32) {
+    a.iter_mut().for_each(|x| {
+        let i = i as f32;
+        *x += i;
+    });
 }
