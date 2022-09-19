73ff58b9f0e5ee29f63ec00a9736310933417501
Ren Wang <iamwrm@gmail.com>
2022-09-19 06:02:34 +0000

    bench works

--- hellosimd/src/main.rs
+++ hellosimd/src/main.rs
@@ -4,41 +4,37 @@ use core_simd::*;
 use std::time;
 
 fn main() {
-    // run_simd1();
     run_simd2();
 }
 
+#[allow(dead_code)]
 fn black_box<T>(dummy: T) -> T {
     unsafe { std::ptr::read_volatile(&dummy) }
 }
 
 fn get_vec() -> Vec<f32> {
     let cap = 1000;
-    let mut a = Vec::with_capacity(cap);
-    for i in 0..cap {
-        a.push(i as f32);
-    }
+    let a = vec![0.0; cap];
     a
 }
 
-fn bench<F>(f_to_bench: F)
+fn bench<F>(f_to_bench: F, name: &str, iter_times: u64)
 where
-    F: Fn(&mut Vec<f32>, i32) -> (),
+    F: Fn(&mut Vec<f32>, u64) -> (),
 {
     let mut a = get_vec();
     let t1 = time::Instant::now();
-    for i in 0..10_000_000 {
-        f_to_bench(&mut a, i);
-    }
+    f_to_bench(&mut a, iter_times);
     let idx = 999;
     println!("a[{}] = {}", idx, a[idx]);
-    black_box(a[idx]);
-    println!("simd2: {:?}", t1.elapsed());
+    println!("{}: {:?}\n", name, t1.elapsed());
+    // black_box(a[idx]);
 }
 
 fn run_simd2() {
-    bench(scaler);
-    bench(simd_vector);
+    let iter_time = 1000_000;
+    bench(scaler, "scalar", iter_time);
+    bench(simd_vector, "simd", iter_time);
 }
 
 #[allow(dead_code)]
@@ -58,24 +54,36 @@ fn run_simd1() {
     println!("{:?}", a + b);
 }
 
-fn simd_vector(a: &mut Vec<f32>, i: i32) {
+fn simd_vector(a: &mut Vec<f32>, n_max: u64) {
     let n = a.len() / 16;
 
-    for j in 0..n {
-        let a_slice = &mut a[j * 16..(j + 1) * 16];
+    for simd_slice in 0..n {
+        let a_slice = &mut a[simd_slice * 16..(simd_slice + 1) * 16];
         let mut a16 = f32x16::from_array(a_slice.try_into().unwrap());
-        let i = f32x16::splat(i as f32);
-        a16 = a16 + i;
+
+        let k = (simd_slice * 16..(simd_slice + 1) * 16)
+            .into_iter()
+            .map(|i| i as f32)
+            .collect::<Vec<f32>>();
+        let k16 = f32x16::from_array(k.try_into().unwrap());
+
+        for _ in 0..n_max {
+            a16 = a16 + k16;
+        }
         a_slice.copy_from_slice(&a16.to_array());
     }
 
     for j in n * 16..a.len() {
-        a[j] += i as f32;
+        for _ in 0..n_max {
+            a[j] += j as f32;
+        }
     }
 }
-fn scaler(a: &mut Vec<f32>, i: i32) {
-    a.iter_mut().for_each(|x| {
-        let i = i as f32;
-        *x += i;
+
+fn scaler(a: &mut Vec<f32>, n_max: u64) {
+    a.iter_mut().enumerate().for_each(|(i, x)| {
+        for _ in 0..n_max {
+            *x += i as f32;
+        }
     });
 }
