67328b60e386b4b6bad79a43850f5313f9695e88
Ren Wang <iamwrm@gmail.com>
2022-09-21 06:17:24 +0000

    still working on rdtsc on aarch64

--- src/timer.rs
+++ src/timer.rs
@@ -3,7 +3,7 @@ pub fn get_rdtsc() -> u64 {
     unsafe { core::arch::x86_64::_rdtsc() }
 }
 
+#[cfg(target_arch = "aarch64")]
 pub fn get_rdtsc() -> u64 {
-    // unsafe { CNTVCT_EL0.get() }
     0
 }
