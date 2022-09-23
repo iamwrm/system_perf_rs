713eed29a5dde84e869b6238fee336c25a22f0b6
Ren Wang <iamwrm@gmail.com>
2022-09-23 02:56:11 +0000

    add wasm32 support

--- src/timer.rs
+++ src/timer.rs
@@ -3,7 +3,7 @@ pub fn get_rdtsc() -> u64 {
     unsafe { core::arch::x86_64::_rdtsc() }
 }
 
-#[cfg(target_arch = "aarch64")]
+#[cfg(any(target_arch = "aarch64", target_arch = "wasm32"))]
 pub fn get_rdtsc() -> u64 {
     0
 }
