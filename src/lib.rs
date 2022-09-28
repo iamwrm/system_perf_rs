8a1941331ce2c7ad0f3dfc169d201a51bac152b8
iamwrm <iamwrm@gmail.com>
2022-09-28 14:50:06 +0000

    before bencher

--- src/lib.rs
+++ src/lib.rs
@@ -48,22 +48,20 @@ macro_rules! bench_cl {
     (   $series_func:expr,
         $ans_v:expr,
         $n_v:expr,
-        $iter_time:expr,
         $x:expr,
+        $iter_time:expr,
         $func_name:expr,
         $test_mode:expr
     ) => {{
-        $ans_v.push(
-            bench(
-                || {
-                    let ans: f64 = $n_v.iter().map(|n| $series_func($x, *n)).sum();
-                    black_box(ans);
-                },
-                $iter_time,
-                $func_name,
-                $test_mode,
-            )
-        );
+        $ans_v.push(bench(
+            || {
+                let ans: f64 = $n_v.iter().map(|n| $series_func($x, *n)).sum();
+                black_box(ans);
+            },
+            $iter_time,
+            $func_name,
+            $test_mode,
+        ));
     }};
 }
 
@@ -73,43 +71,11 @@ fn compute_node(n: i32, iter_time: u64, test_mode: &TestMode) -> u128 {
 
     let mut ans_v = vec![];
 
-    bench_cl!(
-        taylor::series_1_over_1mx,
-        ans_v,
-        n_v,
-        iter_time,
-        x,
-        "1/(1-x)",
-        test_mode
-    );
-    bench_cl!(
-        taylor::series_1_over_1m2x,
-        ans_v,
-        n_v,
-        iter_time,
-        x,
-        "1/(1-2x)",
-        test_mode
-    );
-    bench_cl!(taylor::series_e, ans_v, n_v, iter_time, x, "e^x", test_mode);
-    bench_cl!(
-        taylor::series_cos,
-        ans_v,
-        n_v,
-        iter_time,
-        x,
-        "cos(x)",
-        test_mode
-    );
-    bench_cl!(
-        taylor::series_sin,
-        ans_v,
-        n_v,
-        iter_time,
-        x,
-        "sin(x)",
-        test_mode
-    );
+    bench_cl! { taylor::series_1_over_1mx, ans_v, n_v, x, iter_time, "1/(1-x)", test_mode };
+    bench_cl! { taylor::series_1_over_1m2x, ans_v, n_v, x, iter_time, "1/(1-2x)", test_mode };
+    bench_cl! { taylor::series_e, ans_v, n_v, x, iter_time, "e^x", test_mode};
+    bench_cl! { taylor::series_cos, ans_v, n_v, x, iter_time, "cos(x)", test_mode };
+    bench_cl! { taylor::series_sin, ans_v, n_v, x, iter_time, "sin(x)", test_mode };
 
     let mean = geometric_mean(&ans_v);
     if let TestMode::SingleThread = test_mode {
